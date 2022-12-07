0278a65c55805fb434d18dcda6e6f8e2feba736f
Nikita Podshivalov <psihachina@gmail.com>
2022-12-07 12:28:51 +0300

    added validation and callback to turbine model

--- app/models/turbine.rb
+++ app/models/turbine.rb
@@ -1,2 +1,12 @@
 class Turbine < ApplicationRecord
+  validates :maximum_power, :rotor_diameter, numericality: {greater_than: 0, allow_nil: true }
+  validates :tower_height, :max_wind_speed, :min_wind_speed, numericality: {greater_than: 0, allow_nil: false }
+  validates :blades, numericality: {greater_than: 1, allow_nil: false }
+  validates :name, presence: true
+
+  after_initialize  { p "initialize"}
+  after_save        { p "save"}
+  after_create      { p "create"}
+  after_update      { p "update"}
+  after_destroy     { p "destroy"}
 end
