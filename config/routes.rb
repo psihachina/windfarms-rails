b699d6e6fdcb7859cc7a92a325aa5ed95e11eace
Nikita Podshivalov <psihachina@gmail.com>
2022-12-07 13:37:37 +0300

    added turbines route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # get 'turbines', to: 'turbines#index'
+  # post 'turbines', to: 'turbines#create'
+  resource :turbine
 end
